9b353653d4fbc71445caa0cc6bc07464a43bd8a6
julio cesar alves aaujo fuganti <juliocesarfuganti@gmail.com>
2026-09-03 21:58:12 -0300

    Initial Structure

---
+++ src/graph/main_graph.py
@@ -0,0 +1,32 @@
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langgraph.graph import StateGraph, END
+from typing import TypedDict, List
+
+class AgentState(TypedDict):
+    input: str
+    output: str
+    tool_calls: list[dict]
+    tool_results: list[dict]
+    next_step: str
+    retries: int
+
+class JobGraph:
+    def __init__(self, api_key: str, ai_model: str, tools: List[str], **kwargs):
+        self.ai_model: str = ai_model
+        self.api_key: str = api_key
+        self.possible_tools: List[str] = tools
+        self.graph = None
+
+    def create_graph(self):
+        builder = StateGraph(AgentState)
+
+        builder.add_node()
+        builder.add_node()
+
+        builder.set_entry_point() 
+
+        builder.add_edge()
+        builder.add_edge()
+
+        return builder.compile()
